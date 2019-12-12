a61dbeb84edaea46b8b93d0301429186ff956c83
Luke Liu <liuluke0325@gmail.com>
2019-12-12 13:41:36 -0500

    Initial Commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Hsueh Chih Liu on 2019/12/12.
+//  Copyright © 2019 Luke Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
